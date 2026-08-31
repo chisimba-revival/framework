5c2f24febe5265d4f34348f26d17b2c5012f608e
Derek Keats <derek@dkeats.com>
2026-08-31 13:55:03 +0200

    Add bare icon button skin primitive

--- app/skins/chisimba-reborn/tests/knowledge_workspace_primitives_contract_test.php
+++ app/skins/chisimba-reborn/tests/knowledge_workspace_primitives_contract_test.php
@@ -10,6 +10,7 @@ $checks=array(
     'compact button primitive'=>str_contains($stylesheet,'.chisimba-button-compact {'),
     'icon button primitive'=>str_contains($stylesheet,'.chisimba-icon-button {'),
     'small icon button density'=>str_contains($stylesheet,'.chisimba-icon-button--small {'),
+    'bare icon button removes visible container'=>str_contains($stylesheet,'.chisimba-icon-button--bare,')&&str_contains($stylesheet,'border-color: transparent;'),
     'small icon glyph remains visually strong'=>str_contains($stylesheet,'width: 1.28rem;')&&str_contains($stylesheet,'stroke-width: 2.35;'),
     'toolbar primitive'=>str_contains($stylesheet,'.chisimba-toolbar {'),
     'popover primitive'=>str_contains($stylesheet,'.chisimba-popover {'),
